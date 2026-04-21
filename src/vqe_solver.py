83f2e3377a035822214ddaa898649d8c1398bd2f
zhanghongfeng <tankzhang19@gmail.com>
2026-04-21 22:26:26 +0200

    new parameter to input backend

--- src/vqe_solver.py
+++ src/vqe_solver.py
@@ -4,12 +4,16 @@ from qiskit.primitives import Sampler
 from qiskit.circuit.library import EfficientSU2
 
 
-def get_min(qubit_op, vqe_config):
+def get_min(qubit_op, vqe_config, sampler=None):
+
+    if sampler is None:
+        sampler = Sampler()
+
     optimizer = COBYLA(maxiter=vqe_config.get('maxiter', 50))
     ansatz = EfficientSU2(
         su2_gates=['rx'],
-        entanglement=vqe_config.get('entanglement', 'circular'),
-        reps=vqe_config.get('reps', 2)
+        entanglement=vqe_config['ansatz'].get('entanglement', 'circular'),
+        reps=vqe_config['ansatz'].get('reps', 2)
     )
 
     counts = []
@@ -22,7 +26,7 @@ def get_min(qubit_op, vqe_config):
     # initialize VQE using CVaR with alpha = 0.1 or 0.05 which can be set by user,
     # this method set alpha = 0.1
     vqe = SamplingVQE(
-        Sampler(),
+        sampler,
         ansatz=ansatz,
         optimizer=optimizer,
         aggregation=vqe_config.get('alpha', 0.1),
@@ -30,4 +34,5 @@ def get_min(qubit_op, vqe_config):
     )
     raw_result = vqe.compute_minimum_eigenvalue(qubit_op)
     # We only need the final qubit and the lowest value
-    return raw_result.best_measurement['bitstring'], raw_result.best_measurement['value']
+    return raw_result.best_measurement['bitstring'], raw_result.best_measurement['value'].real
+
