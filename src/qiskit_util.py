78f01b9a9fe9a2154dc940315162df9df3c746fd
zhanghongfeng <tankzhang19@gmail.com>
2026-04-29 08:23:49 +0200

    new estimate qubit function

--- src/qiskit_util.py
+++ src/qiskit_util.py
@@ -3,8 +3,14 @@ from qiskit_aer.noise import NoiseModel
 from qiskit_ibm_runtime.fake_provider import FakeSherbrooke, FakeManila, FakeSydney, FakeOslo, FakeTokyo
 from qiskit_aer.primitives import Sampler
 from qiskit import transpile
+
 warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+from denseCodon import DenseCodon
+from oneHotCodon import OneHotCodon
+from hamiltonian import CodonOptimizer
+import util
 
 _FAKE_BACKENDS = {
     "FakeManila": FakeManila,
@@ -52,3 +58,23 @@ def analyze_circuit(circuit, backend_name):
     depth = transpiled_circuit.depth()
 
     return ops.values(), depth
+
+
+def estimate_qubits(sequence, config, encode_type):
+    if encode_type == 'dense':
+        codon = CodonOptimizer(sequence, config, DenseCodon, encode_type)
+    else:
+        codon = CodonOptimizer(sequence, config, OneHotCodon, "one-hot")
+
+    return codon.qubit_len
+
+
+def estimate_qubits_from_protein(protein, chunk_size, config, encode_type):
+    sequences = util.split_sequence(protein, chunk_size)
+
+    qubits_list = []
+    for seq in sequences:
+        qubit_len = estimate_qubits(seq, config, encode_type)
+        qubits_list.append(qubit_len)
+
+    return qubits_list
