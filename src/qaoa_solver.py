c3054a45ac34a8be0bc792ec03c6d89bd0e606d2
zhanghongfeng <tankzhang19@gmail.com>
2026-08-19 11:59:23 +0200

    qaoa update

--- src/qaoa_solver.py
+++ src/qaoa_solver.py
@@ -1,11 +1,9 @@
 from qiskit_algorithms import QAOA
 from qiskit_algorithms.optimizers import COBYLA
 from qiskit.primitives import StatevectorSampler as Sampler
-from qiskit.circuit.library import EfficientSU2
 import numpy as np
-from qiskit_aer import AerSimulator
 from qiskit import transpile
-
+from qiskit.circuit.library import QAOAAnsatz
 
 class QAOACustomAnsatz(QAOA):
     """QAOA subclass that allows a fixed, user-supplied ansatz instead of
@@ -17,13 +15,11 @@ class QAOACustomAnsatz(QAOA):
 def get_min(qubit_op, qaoa_config, sampler=None, fake_backend=None):
     if sampler is None:
         sampler = Sampler()
-
+    reps = 2
     optimizer = COBYLA(maxiter=qaoa_config.get('maxiter', 50))
-    ansatz = EfficientSU2(
-        num_qubits=qubit_op.num_qubits,
-        su2_gates=['rx'],
-        entanglement=qaoa_config['ansatz'].get('entanglement', 'circular'),
-        reps=qaoa_config['ansatz'].get('reps', 2)
+    ansatz = QAOAAnsatz(
+        cost_operator=qubit_op,
+        reps=reps
     ).decompose(reps=3)
 
 
@@ -31,14 +27,14 @@ def get_min(qubit_op, qaoa_config, sampler=None, fake_backend=None):
     qaoa.ansatz = ansatz
     result = qaoa.compute_minimum_eigenvalue(qubit_op)
 
-    optimal_circuit = qaoa.ansatz.assign_parameters(result.optimal_parameters)
+    optimal_circuit = ansatz.assign_parameters(result.optimal_parameters)
     # Transpile for backend
     if fake_backend is not None and qubit_op.num_qubits > fake_backend.configuration().n_qubits:
         print(f"Warning: {qubit_op.num_qubits} qubits exceeds backend capacity "
               f"({fake_backend.configuration().n_qubits}); transpiling without backend constraints.")
-        transpiled = transpile(optimal_circuit, optimization_level=2)
+        transpiled = transpile(optimal_circuit, optimization_level=1)
     else:
-        transpiled = transpile(optimal_circuit, backend=fake_backend, optimization_level=2)
+        transpiled = transpile(optimal_circuit, backend=fake_backend, optimization_level=1)
     # Gate statistics
     statistics = [transpiled.count_ops(), sum(transpiled.count_ops().values()), transpiled.depth()]
 
